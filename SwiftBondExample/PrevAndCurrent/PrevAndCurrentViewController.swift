d7bb08ab658dc4003f6390602ae504001ce7b053
akio0911 <akio0911@gmail.com>
2016-05-28 01:29:55 +0900

    Add PrevAndCurrentViewModel and PrevAndCurrentViewController

--- SwiftBondExample/PrevAndCurrent/PrevAndCurrentViewController.swift
+++ SwiftBondExample/PrevAndCurrent/PrevAndCurrentViewController.swift
@@ -7,29 +7,56 @@
 //
 
 import UIKit
+import Bond
+
+private class PrevAndCurrentViewModel {
+    let current: Observable<Int?>
+    
+    typealias PrevAndCurrent = (prev: Int?, current: Int?)
+    
+    let prevAndCurrent: EventProducer<PrevAndCurrent>
+    
+    let prevText:    EventProducer<String>
+    let currentText: EventProducer<String>
+    
+    init() {
+        current = Observable<Int?>(nil)
+
+        prevAndCurrent = current.reduce((nil,nil)) { pair, new in
+            return (pair.1, new)
+        }
+        
+        prevText    = prevAndCurrent.map{ $0.prev?   .description ?? "nil" }
+        currentText = prevAndCurrent.map{ $0.current?.description ?? "nil" }
+    }
+    
+    func updateNum() {
+        current.value = Int(arc4random_uniform(9)) + 1
+    }
+}
 
 class PrevAndCurrentViewController: UIViewController {
-
+    
+    private let viewModel = PrevAndCurrentViewModel()
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var prevLabel: UILabel!
+    @IBOutlet weak var currentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nextButton.bnd_tap.observe( viewModel.updateNum )
+        
+        // fix #242: startWith not working #252
+        // https://github.com/SwiftBond/Bond/pull/252
+        
+        prevLabel   .text = "nil"
+        currentLabel.text = "nil"
+        
+        viewModel.prevText   .bindTo(prevLabel   .bnd_text)
+        viewModel.currentText.bindTo(currentLabel.bnd_text)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
