11cc367c4d23135330c457c672531b9a52758303
akio0911 <akio0911@gmail.com>
2016-05-28 04:14:22 +0900

    Add ViewModel

--- SwiftBondExample/TabColor/ViewController.swift
+++ SwiftBondExample/TabColor/ViewController.swift
@@ -7,8 +7,27 @@
 //
 
 import UIKit
+import Bond
+
+internal class ViewModel {
+    internal let color = ColorManager.sharedManager.color.map{ $0 }
+    
+    internal func changeToRed() {
+        ColorManager.sharedManager.color.value = .Red
+    }
+    
+    internal func changeToGreen() {
+        ColorManager.sharedManager.color.value = .Green
+    }
+
+    internal func changeToBlue() {
+        ColorManager.sharedManager.color.value = .Blue
+    }
+}
 
 class ViewController: UIViewController {
+    
+    private let viewModel = ViewModel()
     
     @IBOutlet weak var redButton: UIButton!
     @IBOutlet weak var greenButton: UIButton!
@@ -17,21 +36,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ColorManager.sharedManager.color.observe { [weak self] color in
+        viewModel.color.observe { [weak self] color in
             self?.view.backgroundColor = color.color
         }
-        
-        redButton.bnd_tap.observe {
-            ColorManager.sharedManager.color.value = .Red
-        }
-        
-        greenButton.bnd_tap.observe {
-            ColorManager.sharedManager.color.value = .Green
-        }
-        
-        blueButton.bnd_tap.observe {
-            ColorManager.sharedManager.color.value = .Blue
-        }
+
+        redButton  .bnd_tap.observe( viewModel.changeToRed   )
+        greenButton.bnd_tap.observe( viewModel.changeToGreen )
+        blueButton .bnd_tap.observe( viewModel.changeToBlue  )
     }
 }
 
